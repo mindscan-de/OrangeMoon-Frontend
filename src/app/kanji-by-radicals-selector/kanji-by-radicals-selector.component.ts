47bf05eb316a3e89ade7510fad6c75ba3eaf4836
mindscan-de <maxim.gansert@gmail.com>
2021-03-21 16:03:55 +0100

    implement a simple way to show the backend service kanji radicals

--- src/app/kanji-by-radicals-selector/kanji-by-radicals-selector.component.ts
+++ src/app/kanji-by-radicals-selector/kanji-by-radicals-selector.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import {KanjiDataBackendService} from '../backend-service/kanji-data-backend.service';
+
+import { BackendKanjiRadicals } from '../backend-service/backend-model/backend-kanji-radicals';
 
 @Component({
   selector: 'app-kanji-by-radicals-selector',
@@ -6,10 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kanji-by-radicals-selector.component.css']
 })
 export class KanjiByRadicalsSelectorComponent implements OnInit {
+	
+	public radicals: BackendKanjiRadicals = new BackendKanjiRadicals();
 
-  constructor() { }
+	constructor(private activatedRoute : ActivatedRoute, private backendService: KanjiDataBackendService, ) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.retrieveRadicals();
+	}
+	
+    retrieveRadicals() {
+        this.backendService.getKanjiRadicals().subscribe(
+			data => this.onKanjiRadicalsLoaded(data),
+			error => this.onKanjiRadicalsFailes(error)
+		);
+    }
+
+    onKanjiRadicalsLoaded(data: BackendKanjiRadicals): void {
+        this.radicals = data;
+    }
+
+    onKanjiRadicalsFailes(error: any): void {
+        console.log(error);
+    }
 
 }
