08b159ab380ff9b194704b2c25b91580273f0903
mindscan-de <maxim.gansert@gmail.com>
2021-03-21 14:58:45 +0100

    add main footer to the Fronten project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainFooterComponent } from './main-footer/main-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainFooterComponent
   ],
   imports: [
     BrowserModule,
