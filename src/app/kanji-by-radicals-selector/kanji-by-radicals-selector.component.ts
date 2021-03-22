5b8d4f80f3e07a6fe984f9308f31a248ab293907
mindscan-de <maxim.gansert@gmail.com>
2021-03-22 21:51:51 +0100

    try to retrieve the selected kanji.

--- src/app/kanji-by-radicals-selector/kanji-by-radicals-selector.component.ts
+++ src/app/kanji-by-radicals-selector/kanji-by-radicals-selector.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 
 import {KanjiDataBackendService} from '../backend-service/kanji-data-backend.service';
 
+import { BackendKanjiData } from '../backend-service/backend-model/backend-kanji-data';
 import { BackendKanjiRadicals } from '../backend-service/backend-model/backend-kanji-radicals';
 
 @Component({
@@ -13,12 +14,15 @@ import { BackendKanjiRadicals } from '../backend-service/backend-model/backend-k
 export class KanjiByRadicalsSelectorComponent implements OnInit {
 	
 	public radicals: BackendKanjiRadicals = new BackendKanjiRadicals();
+	public retrievedKanji: BackendKanjiData = new BackendKanjiData();
 
 	constructor(private activatedRoute : ActivatedRoute, private backendService: KanjiDataBackendService, ) { }
 
 	ngOnInit(): void {
 		this.retrieveRadicals();
+		this.retrieveKanjiByRadicals(['\u96E8','\u6728']);
 	}
+	
 	
     retrieveRadicals() {
         this.backendService.getKanjiRadicals().subscribe(
@@ -26,6 +30,20 @@ export class KanjiByRadicalsSelectorComponent implements OnInit {
 			error => this.onKanjiRadicalsFailes(error)
 		);
     }
+
+    retrieveKanjiByRadicals(selection:Array<String>) {
+        this.backendService.getKanjiByRadicals(selection).subscribe(
+			data => this.onSelectedKanjiLoaded(data),
+			error => this.onKanjiRadicalsFailes(error)
+		);
+    }
+
+    onSelectedKanjiLoaded(data: BackendKanjiData): void {
+		console.log(data);
+	
+        this.retrievedKanji = data;
+    }
+
 
     onKanjiRadicalsLoaded(data: BackendKanjiRadicals): void {
         this.radicals = data;
