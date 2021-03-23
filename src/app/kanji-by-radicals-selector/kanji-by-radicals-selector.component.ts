deb1020f1cde79ffed68d2b3b953b8d1410d4fd9
mindscan-de <maxim.gansert@gmail.com>
2021-03-23 21:25:36 +0100

    show current buttonstate for associated selectable kanji

--- src/app/kanji-by-radicals-selector/kanji-by-radicals-selector.component.ts
+++ src/app/kanji-by-radicals-selector/kanji-by-radicals-selector.component.ts
@@ -6,6 +6,9 @@ import {KanjiDataBackendService} from '../backend-service/kanji-data-backend.ser
 import { BackendKanjiData } from '../backend-service/backend-model/backend-kanji-data';
 import { BackendKanjiRadicals } from '../backend-service/backend-model/backend-kanji-radicals';
 
+import { UiModelSelectableKanji }  from './ui-model/ui-model-selectable-kanji';
+
+
 @Component({
   selector: 'app-kanji-by-radicals-selector',
   templateUrl: './kanji-by-radicals-selector.component.html',
@@ -15,6 +18,11 @@ export class KanjiByRadicalsSelectorComponent implements OnInit {
 	
 	public radicals: BackendKanjiRadicals = new BackendKanjiRadicals();
 	public retrievedKanji: BackendKanjiData = new BackendKanjiData();
+	
+    public selectableRadicals: UiModelSelectableKanji[] = new Array<UiModelSelectableKanji>();
+
+	public combinedRadicals: Set<String> = new Set<String>();
+	
 
 	constructor(private activatedRoute : ActivatedRoute, private backendService: KanjiDataBackendService, ) { }
 
@@ -46,11 +54,28 @@ export class KanjiByRadicalsSelectorComponent implements OnInit {
 
 
     onKanjiRadicalsLoaded(data: BackendKanjiRadicals): void {
+		let selectableRadicals = new Array<UiModelSelectableKanji>();
+		for(let i=0;i<data.values.length;i++) {
+			let radical = new UiModelSelectableKanji(data.values[i]);
+			selectableRadicals.push(radical);
+		}
+		
+		this.selectableRadicals = selectableRadicals;
         this.radicals = data;
     }
 
     onKanjiRadicalsFailes(error: any): void {
         console.log(error);
     }
+
+	toggleRadical(radical:UiModelSelectableKanji): void {
+		
+		radical.selected = !radical.selected;
+		
+		console.log(radical);
+		
+		// TODO: Combine the selected radicals
+		// button.selected = this.selectedRadicals[index]?"selected":"";
+	}
 
 }
