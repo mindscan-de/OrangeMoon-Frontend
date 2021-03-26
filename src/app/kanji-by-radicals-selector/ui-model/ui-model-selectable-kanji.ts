478653b6031b21554b7c7664e0859ced5d686a09
mindscan-de <maxim.gansert@gmail.com>
2021-03-26 20:27:42 +0100

    extend the selectable kanji ui model by a disabled property.

--- src/app/kanji-by-radicals-selector/ui-model/ui-model-selectable-kanji.ts
+++ src/app/kanji-by-radicals-selector/ui-model/ui-model-selectable-kanji.ts
@@ -1,8 +1,13 @@
 export class UiModelSelectableKanji {
 	public selected:boolean = false;
-	public kanji: String = "";
+	public kanji: String = '';
+	public disabled: String = '';
 	
 	constructor(kanji:String) {
 		this.kanji = kanji;
+	}
+	
+	setDisabled(newValue:boolean) {
+		this.disabled = newValue==true?'disabled':'';
 	}
 }
