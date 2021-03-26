24b8693c68e182b99461755f49363c26e48b36d2
mindscan-de <maxim.gansert@gmail.com>
2021-03-26 20:15:37 +0100

    add combined datatype to retrieve the kanji and radicals in one request

---
+++ src/app/backend-service/backend-model/backend-kanji-and-radical-data.spec.ts
@@ -0,0 +1,7 @@
+import { BackendKanjiAndRadicalData } from './backend-kanji-and-radical-data';
+
+describe('BackendKanjiAndRadicalData', () => {
+  it('should create an instance', () => {
+    expect(new BackendKanjiAndRadicalData()).toBeTruthy();
+  });
+});
---
+++ src/app/backend-service/backend-model/backend-kanji-and-radical-data.ts
@@ -0,0 +1,4 @@
+export class BackendKanjiAndRadicalData {
+	public values: Array<String> = [];
+	public remaining_radicals: Array<String> = [];
+}
