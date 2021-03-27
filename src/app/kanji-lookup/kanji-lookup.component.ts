1e182eb07df4c1d1feb71d587d2c5f5990da689b
mindscan-de <maxim.gansert@gmail.com>
2021-03-27 15:43:56 +0100

    subscribe to query string - route to query parameter

--- src/app/kanji-lookup/kanji-lookup.component.ts
+++ src/app/kanji-lookup/kanji-lookup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, Params} from '@angular/router';
 
 @Component({
   selector: 'app-kanji-lookup',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kanji-lookup.component.css']
 })
 export class KanjiLookupComponent implements OnInit {
+	
+	public currentKanjiQueryString: String="test";
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+	constructor(private activatedRoute: ActivatedRoute, private router: Router) {
+  		// subscribe to page parameters		
+  		this.activatedRoute.queryParams.subscribe( pageParameters => {
+	 		this.onContentPageParametersProvided( pageParameters );
+  		});
+	}
+	
+	ngOnInit(): void {
+  		this.activatedRoute.queryParams.subscribe( pageParameters => {
+	 		this.onContentPageParametersProvided( pageParameters );
+  		});
+	  	
+	}
+	
+	onContentPageParametersProvided( pageParameters ):void { 
+		this.currentKanjiQueryString = this.activatedRoute.snapshot.queryParams["q"];
+	}
+	
+	onSubmit():void {
+		var queryParameterQ = this.currentKanjiQueryString;
+		this.router.navigate( [ '/kanjilookup'], { queryParams:{q:queryParameterQ } });
+	}
 
 }
