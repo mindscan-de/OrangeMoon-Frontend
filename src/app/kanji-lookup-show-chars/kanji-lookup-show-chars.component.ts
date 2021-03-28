a6a3f007eac3961be48b2bc5f34deee3aeb93743
mindscan-de <maxim.gansert@gmail.com>
2021-03-28 20:00:02 +0200

    start simple ui-implementation baseline

--- src/app/kanji-lookup-show-chars/kanji-lookup-show-chars.component.ts
+++ src/app/kanji-lookup-show-chars/kanji-lookup-show-chars.component.ts
@@ -1,5 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, SimpleChanges } from '@angular/core';
 
+// Backend Model
+import { BackendLookupResultChar} from '../backend-service/backend-model/backend-lookup-result'
+
+// UI Model
+import { UiLookupResultChar } from './ui-model/ui-lookup-result-char';
+
+// M2M transforamation from BackendModel to UI-Model
+import { M2mKanjiLookupService } from '../m2m/m2m-kanji-lookup.service';
+
+/**
+ * This component is only for processing the chars-entries and presenting them
+ */
 @Component({
   selector: 'app-kanji-lookup-show-chars',
   templateUrl: './kanji-lookup-show-chars.component.html',
@@ -7,9 +19,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KanjiLookupShowCharsComponent implements OnInit {
 
-  constructor() { }
+	public uiChars: UiLookupResultChar[] = [];
 
-  ngOnInit(): void {
-  }
+	constructor(private m2m: M2mKanjiLookupService) { }
+
+	ngOnInit(): void {
+	}
+
+	@Input() 
+	public set chars(chars: BackendLookupResultChar[]) {
+		let uiChars: UiLookupResultChar[] = new Array<UiLookupResultChar>(chars.length);
+		
+		this.uiChars = uiChars;
+	}
 
 }
---
+++ src/app/kanji-lookup-show-chars/ui-model/ui-lookup-result-char.spec.ts
@@ -0,0 +1,7 @@
+import { UiLookupResultChar } from './ui-lookup-result-char';
+
+describe('UiLookupResultChar', () => {
+  it('should create an instance', () => {
+    expect(new UiLookupResultChar()).toBeTruthy();
+  });
+});
