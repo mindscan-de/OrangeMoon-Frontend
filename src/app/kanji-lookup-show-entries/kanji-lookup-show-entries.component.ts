8022a6b34a1a1f86d0dce9d488fbf2fe99862815
mindscan-de <maxim.gansert@gmail.com>
2021-03-28 16:31:11 +0200

    replace entries presentation into a separate component

--- src/app/kanji-lookup-show-entries/kanji-lookup-show-entries.component.ts
+++ src/app/kanji-lookup-show-entries/kanji-lookup-show-entries.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, SimpleChanges } from '@angular/core';
 
+import { BackendLookupResultEntry } from '../backend-service/backend-model/backend-lookup-result' 
+
+
+/**
+ * This component is only for processing the lookup-entries and present them.
+ */
 @Component({
   selector: 'app-kanji-lookup-show-entries',
   templateUrl: './kanji-lookup-show-entries.component.html',
   styleUrls: ['./kanji-lookup-show-entries.component.css']
 })
-export class KanjiLookupShowEntriesComponent implements OnInit {
+export class KanjiLookupShowEntriesComponent implements OnInit, OnChanges {
+	
+	public thisEntries: BackendLookupResultEntry[] = [];
+	
+	constructor() { }
+	
+	ngOnInit(): void {
+	}
+	
+	@Input() 
+	public set entries(entries: BackendLookupResultEntry[]) {
+		this.thisEntries = entries;
+		{
+			// TODO: we might convert that into a ui-model
+			console.log("entries set- printing entries");
+			console.log(entries);
+		}
+	}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+	ngOnChanges(changes:SimpleChanges) :void {
+		if(changes.entries) {
+			// TODO: we might convert that into a ui-model
+			console.log("entries changed - printing entries");
+			console.log(changes.entries);
+		}
+	}
 
 }
