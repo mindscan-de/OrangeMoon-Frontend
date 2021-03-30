6c78110a3bbf19bf7f8e7df36157afb9db38d2e2
mindscan-de <maxim.gansert@gmail.com>
2021-03-30 18:40:12 +0200

    add routing and navigation to the Kanji/Kana Translation Memory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { KanjiByRadicalsSelectorComponent } from './kanji-by-radicals-selector/kanji-by-radicals-selector.component';
-import { KanjiLookupComponent } from './kanji-lookup/kanji-lookup.component'
+import { KanjiLookupComponent } from './kanji-lookup/kanji-lookup.component';
+import { KanjiTMComponent } from './kanji-tm/kanji-tm.component';
 import { MainComponent } from './main/main.component';
 
 
@@ -9,7 +10,8 @@ const routes: Routes = [
 	{ path: '', component:MainComponent},
 	{ path: 'main', component:MainComponent},
 	{ path: 'kanjibyradicals', component:KanjiByRadicalsSelectorComponent},
-	{ path: 'kanjilookup', component:KanjiLookupComponent}	
+	{ path: 'kanjilookup', component:KanjiLookupComponent},
+	{ path: 'ktm', component:KanjiTMComponent}
 ];
 
 @NgModule({
