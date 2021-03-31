2118db92989742e7f77b6b6c63f16078ed1aea17
mindscan-de <maxim.gansert@gmail.com>
2021-03-31 12:49:59 +0200

    add ui data type properties

--- src/app/kanji-lookup-show-names/ui-model/ui-lookup-result-name.ts
+++ src/app/kanji-lookup-show-names/ui-model/ui-lookup-result-name.ts
@@ -1,2 +1,31 @@
 export class UiLookupResultName {
+	public idseq: String;
+	
+	public mainKanji: String="";
+	public mainKana: String="";
+	public senses: Array<UiLookupResultNameSense> = new Array<UiLookupResultNameSense>();
+	
+	constructor(idseq:String) {
+		this.idseq = idseq;
+	}
+	
+	setMainKanjiData(mainKanji: String, mainKana: String) : void {
+		this.mainKana = mainKana;
+		this.mainKanji = mainKanji;
+	}
+	
+	addSense ( glosstext: String, type: String) : void {
+		let newSense : UiLookupResultNameSense = new UiLookupResultNameSense();
+		
+		newSense.translation = glosstext;
+		newSense.type = type;
+		
+		this.senses.push(newSense);
+	}
+	
+}
+
+export class UiLookupResultNameSense {
+	public translation: String;
+	public type: String; 
 }
