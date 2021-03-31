4a75a4e58d63ea9c0effb1125126701199268df6
mindscan-de <maxim.gansert@gmail.com>
2021-03-31 11:56:19 +0200

    add skeleton of the show names component

--- src/app/kanji-lookup-show-names/kanji-lookup-show-names.component.ts
+++ src/app/kanji-lookup-show-names/kanji-lookup-show-names.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, SimpleChanges } from '@angular/core';
 
+// Backend Model
+import { BackendLookupResultName } from '../backend-service/backend-model/backend-lookup-result'
+
+// UI Model
+import { UiLookupResultName } from './ui-model/ui-lookup-result-name';
+
+// M2M transforamation from BackendModel to UI-Model
+import { M2mKanjiLookupService } from '../m2m/m2m-kanji-lookup.service';
+
+/**
+ * This component is only for processing the names-entries and presenting them. 
+ */
 @Component({
   selector: 'app-kanji-lookup-show-names',
   templateUrl: './kanji-lookup-show-names.component.html',
   styleUrls: ['./kanji-lookup-show-names.component.css']
 })
 export class KanjiLookupShowNamesComponent implements OnInit {
+	
+	public uiNames: UiLookupResultName[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+	constructor(private m2m: M2mKanjiLookupService) { }
+	
+	ngOnInit(): void {
+	}
+	
+	@Input()
+	public set names(names: BackendLookupResultName[]) {
+		let uiNames: UiLookupResultName[] = new Array<UiLookupResultName>(names.length);
+		
+		this.uiNames = uiNames;
+	}
+	
+	ngOnChanges(changes:SimpleChanges) : void {
+		// ignore yet (changes.names??)
+	}
 
 }
---
+++ src/app/kanji-lookup-show-names/ui-model/ui-lookup-result-name.spec.ts
@@ -0,0 +1,7 @@
+import { UiLookupResultName } from './ui-lookup-result-name';
+
+describe('UiLookupResultName', () => {
+  it('should create an instance', () => {
+    expect(new UiLookupResultName()).toBeTruthy();
+  });
+});
