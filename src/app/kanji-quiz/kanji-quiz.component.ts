df2eaab6507e15f047ae42226f5db65108e15339
mindscan-de <maxim.gansert@gmail.com>
2021-04-02 23:05:34 +0200

    add simple method skeletons to trigger thinking about quiz game impl.

--- src/app/kanji-quiz/kanji-quiz.component.ts
+++ src/app/kanji-quiz/kanji-quiz.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KanjiQuizComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
+	onViewQuizLists() : void {
+		// will show the list of quizes
+		// will allow to show the quiz data
+		
+		// will allow private quiz games and continue 
+		// * maybe use createQuizGame or JoinQuizGame 
+	}
+	
+	onCreateQuizGame() : void {
+		// Will let one player create a new game, and register the game, create a temporary user name
+		// Will be the one who kicks off the game, if everyone is in
+	}
+	
+	onJoinQuizGame() : void {
+		// Let the user input a token, and then create a new username
+		// you can join late, but you will not be able to replay the early answers
+	}
 }
