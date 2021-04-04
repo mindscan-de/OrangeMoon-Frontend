a16e4a3df00bf0dbfaa42b6b120882348d6616c1
mindscan-de <maxim.gansert@gmail.com>
2021-04-04 13:37:14 +0200

    add UI-Model for the Show Kanji-Quiz Dialog

---
+++ src/app/kanji-quiz/show-quiz-data-dialog/ui-model/ui-quiz-data.spec.ts
@@ -0,0 +1,7 @@
+import { UiQuizData } from './ui-quiz-data';
+
+describe('UiQuizData', () => {
+  it('should create an instance', () => {
+    expect(new UiQuizData()).toBeTruthy();
+  });
+});
---
+++ src/app/kanji-quiz/show-quiz-data-dialog/ui-model/ui-quiz-data.ts
@@ -0,0 +1,11 @@
+export class UiQuizData {
+	public name: String = "";
+	public length: Number = 0;
+	public list: UiQuizDataItem[] = [];
+}
+
+export class UiQuizDataItem {
+	public kanji: String = "";
+	public kana: String = "";
+	public meaning: String = "";
+}
