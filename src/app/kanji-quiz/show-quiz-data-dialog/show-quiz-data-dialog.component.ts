efbc2e3d8b547d6cee69ef5be64e222caad4e714
mindscan-de <maxim.gansert@gmail.com>
2021-04-04 13:45:34 +0200

    start with skeleton of showing the quiz data

--- src/app/kanji-quiz/show-quiz-data-dialog/show-quiz-data-dialog.component.ts
+++ src/app/kanji-quiz/show-quiz-data-dialog/show-quiz-data-dialog.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
+// import Backend
 import { KanjiDataBackendService } from '../../backend-service/kanji-data-backend.service';
 
+// import UI-Model
+import { UiQuizData, UiQuizDataItem } from './ui-model/ui-quiz-data';
+
+
+// TODO: 
+// will allow to show the quiz data
+// will allow private quiz games (and continue?) 
 
 @Component({
   selector: 'app-show-quiz-data-dialog',
@@ -10,15 +18,16 @@ import { KanjiDataBackendService } from '../../backend-service/kanji-data-backen
   styleUrls: ['./show-quiz-data-dialog.component.css']
 })
 export class ShowQuizDataDialogComponent  {
+	
+	public uiQuizData : UiQuizData = new UiQuizData();
 
 	constructor(public activeModal: NgbActiveModal, private backendService : KanjiDataBackendService) { }
 
-	setDialogData( quiz_name : String ): void {
-		// TODO subscribe to the kanji data...
-		
-		// will allow to show the quiz data
-		// will allow private quiz games (and continue?) 
-		
+	setDialogData( quiz_name : string ): void {
+		this.backendService.getKanjiQuizData(quiz_name).subscribe(
+			data => {},
+			error => {}
+		);
 	}
 	
 	onClose() : void {
