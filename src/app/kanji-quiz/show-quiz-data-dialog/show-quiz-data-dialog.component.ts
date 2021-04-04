945be4056fa06e0bef2bb11231bea3ef8d680663
mindscan-de <maxim.gansert@gmail.com>
2021-04-04 14:47:15 +0200

    use conversion and show the quiz data.

--- src/app/kanji-quiz/show-quiz-data-dialog/show-quiz-data-dialog.component.ts
+++ src/app/kanji-quiz/show-quiz-data-dialog/show-quiz-data-dialog.component.ts
@@ -3,6 +3,11 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 // import Backend
 import { KanjiDataBackendService } from '../../backend-service/kanji-data-backend.service';
+import { BackendQuizData } from '../../backend-service/backend-model/backend-quiz-data';
+
+// import M2M
+
+import { M2mKanjiLookupService } from '../../m2m/m2m-kanji-lookup.service';
 
 // import UI-Model
 import { UiQuizData, UiQuizDataItem } from './ui-model/ui-quiz-data';
@@ -21,13 +26,20 @@ export class ShowQuizDataDialogComponent  {
 	
 	public uiQuizData : UiQuizData = new UiQuizData();
 
-	constructor(public activeModal: NgbActiveModal, private backendService : KanjiDataBackendService) { }
+	constructor(public activeModal: NgbActiveModal, private backendService : KanjiDataBackendService, private m2m: M2mKanjiLookupService) { }
 
 	setDialogData( quiz_name : string ): void {
 		this.backendService.getKanjiQuizData(quiz_name).subscribe(
-			data => {},
+			data => {
+				let qData: BackendQuizData = data;
+				this.setQuizData( this.m2m.convertQuizData(qData) ) 
+				},
 			error => {}
 		);
+	}
+	
+	setQuizData( quizData : UiQuizData ) : void {
+		this.uiQuizData = quizData;
 	}
 	
 	onClose() : void {
