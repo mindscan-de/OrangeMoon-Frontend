f5022703d905575daff9a3630abf271458d13637
mindscan-de <maxim.gansert@gmail.com>
2021-04-19 21:51:14 +0200

    open the join dialog in case the join game dialog button is clicked

--- src/app/kanji-quiz/kanji-quiz.component.ts
+++ src/app/kanji-quiz/kanji-quiz.component.ts
@@ -6,7 +6,8 @@ import { KanjiDataBackendService } from '../backend-service/kanji-data-backend.s
 import { BackendQuizList, BackendQuizListItem } from '../backend-service/backend-model/backend-quiz-list';
 
 // Modal dialogs
-import { ShowQuizDataDialogComponent } from './show-quiz-data-dialog/show-quiz-data-dialog.component'; 
+import { ShowQuizDataDialogComponent } from './show-quiz-data-dialog/show-quiz-data-dialog.component';
+import { JoinGameDialogComponent } from './join-game-dialog/join-game-dialog.component'; 
 
 @Component({
   selector: 'app-kanji-quiz',
@@ -48,6 +49,14 @@ export class KanjiQuizComponent implements OnInit {
 	}
 	
 	onJoinQuizGame() : void {
+		const modalref = this.modalService.open( JoinGameDialogComponent, {centered: true, ariaLabelledBy: 'modal-basic-title', size:'xl'} );
+		
+		modalref.componentInstance.setDialogDa();
+		
+		modalref.result.then(
+			(result)=>{},
+			(reason)=>{}
+		);
 		// Let the user input a token, and then create a new username
 		// you can join late, but you will not be able to replay the early answers
 	}
