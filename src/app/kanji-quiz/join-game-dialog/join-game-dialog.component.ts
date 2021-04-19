eed38682b2c8b071a0eac9905b7b3d897b0b6d69
mindscan-de <maxim.gansert@gmail.com>
2021-04-19 21:50:31 +0200

    prepare skeleton of the join game dialog.

--- src/app/kanji-quiz/join-game-dialog/join-game-dialog.component.ts
+++ src/app/kanji-quiz/join-game-dialog/join-game-dialog.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-join-game-dialog',
   templateUrl: './join-game-dialog.component.html',
   styleUrls: ['./join-game-dialog.component.css']
 })
-export class JoinGameDialogComponent implements OnInit {
+export class JoinGameDialogComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	constructor(public activeModal: NgbActiveModal) { }
+	
+	setDialogData(  ) : void {
+		
+	}
+	
+	onClose() : void {
+		this.activeModal.close();
+	}
+	
+	onJoin() : void {
+		
+	} 
 }
