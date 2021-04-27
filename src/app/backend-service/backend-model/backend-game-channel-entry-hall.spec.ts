344f85c94319761bbd1bfcd0be15bdc60c9fd476
mindscan-de <maxim.gansert@gmail.com>
2021-04-27 21:20:10 +0200

    add preliminary game channel backend object after creating the channel.

---
+++ src/app/backend-service/backend-model/backend-game-channel-entry-hall.spec.ts
@@ -0,0 +1,7 @@
+import { BackendGameChannelEntryHall } from './backend-game-channel-entry-hall';
+
+describe('BackendGameChannelEntryHall', () => {
+  it('should create an instance', () => {
+    expect(new BackendGameChannelEntryHall()).toBeTruthy();
+  });
+});
---
+++ src/app/backend-service/backend-model/backend-game-channel-entry-hall.ts
@@ -0,0 +1,8 @@
+export class BackendGameChannelEntryHall {
+	public gameChannelId: string;
+	public gameChannelAccessToken: string;
+	
+	public playerName: string;
+	public playerMaster: string;
+	public playerAccessToken: string;
+}
