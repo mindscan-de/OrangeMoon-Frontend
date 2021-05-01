6ee13e2674f9cc01a61f86c2e83b4c38bacfc1de
mindscan-de <maxim.gansert@gmail.com>
2021-05-01 22:19:05 +0200

    load and show the kanji quiz list / kanji quiz game selection

--- src/app/kanji-game/kanji-game.component.ts
+++ src/app/kanji-game/kanji-game.component.ts
@@ -1,16 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, Params} from '@angular/router';
 
+import { KanjiDataBackendService } from '../backend-service/kanji-data-backend.service';
+
+import { BackendQuizList, BackendQuizListItem } from '../backend-service/backend-model/backend-quiz-list';
+
 @Component({
   selector: 'app-kanji-game',
   templateUrl: './kanji-game.component.html',
   styleUrls: ['./kanji-game.component.css']
 })
 export class KanjiGameComponent implements OnInit {
-
-	constructor(private route: ActivatedRoute) { 
-		this.route.params.subscribe(params => console.log(params));
 	
+	public kanjiQuizList: BackendQuizListItem[]= [];
+
+	constructor(private route: ActivatedRoute, private backendService: KanjiDataBackendService) { 
+		this.route.params.subscribe(params => console.log(params));
+		
+		backendService.getKanjiQuizList().subscribe(
+			data => this.onKanjiQuizdataListLoaded(data),
+			error => this.onKanjiQuizdataListFailed(error)
+		);
+	}
+	
+	onKanjiQuizdataListLoaded(listdata : BackendQuizList): void {
+		this.kanjiQuizList = listdata.values;
+	}
+	
+	onKanjiQuizdataListFailed(error:any) : void {
+		console.log(error);
 	}
 
 	ngOnInit(): void {
